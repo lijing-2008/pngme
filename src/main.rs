07d573d410be5566dfdbff0a786d8e1457ca09ca
lijing <jingli14@tsinghua.org.cn>
2022-07-11 12:59:46 +0800

    delete clap example in main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,19 +16,6 @@ mod png;
 mod error;
 mod utils;
 
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[clap(short, long, value_parser)]
-    name: String,
-
-    /// Number of times to greet
-    #[clap(short, long, value_parser, default_value_t = 1)]
-    count: u8,
-}
-
 // cargo run
 fn main() -> Result<(), PngError> {
     let cli = args::Cli::parse();
